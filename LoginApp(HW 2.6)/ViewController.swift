0ac997502f7d2b06110f17d2d1863d7fd4776bef
Artem Potemkin <art.potemkin@gmail.com>
2023-06-06 22:10:55 +0300

    Initial Commit

---
+++ LoginApp(HW 2.6)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp(HW 2.6)
+//
+//  Created by Артём Потёмкин on 06.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
